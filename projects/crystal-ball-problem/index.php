8851789cd2ec2c311eb2e204b1d3048aeeb1dac0
bbq-po-edu <231660485+bbq-pbo-edu@users.noreply.github.com>
2025-09-28 22:22:38 +0200

    chore: extract primitive css variables

--- projects/crystal-ball-problem/index.php
+++ projects/crystal-ball-problem/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/tokens/colors.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Crystal Ball Problem</title>
 </head>
